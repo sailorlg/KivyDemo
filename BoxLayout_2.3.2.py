3fe1b8ff061d63dc19b51361a7d0022aae34d577
sailorlg <sailorlg@163.com>
2021-08-09 15:15:43 +0800

    C_2.3.4

--- BoxLayout_2.3.2.py
+++ BoxLayout_2.3.2.py
@@ -43,24 +43,70 @@ os.environ['KIVY_IMAGE'] = 'pil,sdl2'
 # 	BoxApp().run()
 
 
-from kivy.lang import Builder
-Builder.load_file('./kvs/box.kv')
 
-from kivy.app import App  # 导入Kivy的App类, App类是所有kivy应用的基类
+# # ----------------- 2.3.3 --------------------
+# from kivy.lang import Builder
+# Builder.load_file('./kvs/box.kv')
+#
+# from kivy.app import App  # 导入Kivy的App类, App类是所有kivy应用的基类
+# from kivy.uix.boxlayout import BoxLayout  # 引入布局
+#
+# class BoxLayoutWidget(BoxLayout):  # 布局类
+# 	def __init__(self, **kwargs):  # 初始化
+# 		super().__init__(**kwargs)
+#
+#
+# class BoxApp(App):
+# 	"""
+# 	实现App类的build()方法(继承自App类)
+# 	"""
+# 	def build(self):
+# 		return BoxLayoutWidget()  #返回根控件
+#
+#
+# if __name__ == "__main__":  # 程序入口
+# 	BoxApp().run()  # 启动应用程序
+
+
+
+# --------------------------- 2.3.4 ------------------------
+from kivy.app import App  # 导入kivy的App类, 它是所有kivy应用的基类
+from kivy.uix.button import Button  # 引入控件
 from kivy.uix.boxlayout import BoxLayout  # 引入布局
+from kivy.graphics import Rectangle, Color
+
 
 class BoxLayoutWidget(BoxLayout):  # 布局类
 	def __init__(self, **kwargs):  # 初始化
 		super().__init__(**kwargs)
-		
 
-class BoxApp(App):
-	"""
-	实现App类的build()方法(继承自App类)
-	"""
-	def build(self):
-		return BoxLayoutWidget()  #返回根控件
-	
+		# 设置背景颜色
+		with self.canvas:
+			Color(1, 1, 1, 1)
+
+			self.rect = Rectangle(pos=self.pos, size=self.size)
+			self.bind(pos=self.update_rect, size=self.update_rect)
+		
+		self.layout = BoxLayout(orientation='vertical')  # 创建垂直的BoxLayout
+		self.layout.add_widget(Button(text='Hello'))  # 给BoxLayout添加控件
+		self.layout.add_widget(Button(text='Boxlayout'))  # 给BoxLayout添加控件
+		
+		self.add_widget(self.layout)  # 把BoxLayout作为控件添加到self中
+
+	def update_rect(self, *args):
+		"""
+		设置背景尺寸, 可忽略
+		:param args:
+		:return:
+		"""
+		self.rect.pos = self.pos
+		self.rect.size = self.size
+
+
+class BoxApp(App):  # 继承App类
+	def build(self):  # 实现App类的build()方法
+		return BoxLayoutWidget()  # 返回根控件
+
 
 if __name__ == "__main__":  # 程序入口
-	BoxApp().run()  # 启动应用程序
+	BoxApp().run()
