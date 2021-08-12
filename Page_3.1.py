1d7326d8b6ce02a9f4e4c2d82d933a1215a6ac61
sailorlg <sailorlg@163.com>
2021-08-12 13:23:58 +0800

    C_3.1.1_3

--- Page_3.1.py
+++ Page_3.1.py
@@ -27,37 +27,62 @@
 # 	Page3App().run()
 
 
-# --------------------------------- 2 ----------------------
-import os
+# # --------------------------------- 2 ----------------------
+# import os
+#
+# os.environ['KIVY_IMAGE'] = 'pil,sdl2'
+#
+# from kivy.app import App
+# from kivy.uix.boxlayout import BoxLayout
+# from kivy.graphics import Rectangle, Color
+#
+#
+# class BoxLayoutWidget(BoxLayout):
+# 	def __init__(self, **kwargs):
+# 		super().__init__(**kwargs)
+#
+# 		# 设置背景
+# 		with self.canvas:
+# 			# 设置背景颜色, rgba格式, 通常值为0~1之间(具体的值/255)
+# 			Color(1, 1, 1, 1)
+# 			self.rect = Rectangle(pos=self.pos, size=self.size)
+# 			self.bind(pos=self.update_rect, size=self.update_rect)
+#
+# 	def update_rect(self, *args):
+# 		"""设置背景尺寸"""
+# 		self.rect.pos = self.pos
+# 		self.rect.size = self.size
+#
+#
+# class Page3App(App):
+# 	def build(self):
+# 		return BoxLayoutWidget()
+#
+# if __name__ == "__main__":
+# 	Page3App().run()
 
+
+# ---------------------------- 3 ----------------------------
+import os
 os.environ['KIVY_IMAGE'] = 'pil,sdl2'
+
+
+from kivy.lang import Builder
+Builder.load_file('./kvs/page3.kv')
+
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics import Rectangle, Color
 
 
 class BoxLayoutWidget(BoxLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 		
-		# 设置背景
-		with self.canvas:
-			# 设置背景颜色, rgba格式, 通常值为0~1之间(具体的值/255)
-			Color(1, 1, 1, 1)
-			self.rect = Rectangle(pos=self.pos, size=self.size)
-			self.bind(pos=self.update_rect, size=self.update_rect)
-			
-	def update_rect(self, *args):
-		"""设置背景尺寸"""
-		self.rect.pos = self.pos
-		self.rect.size = self.size
-
 
 class Page3App(App):
 	def build(self):
 		return BoxLayoutWidget()
-
-
+	
 if __name__ == "__main__":
 	Page3App().run()
