d638b54d32baef6ec252346f932728ceed4733d6
sailorlg <sailorlg@163.com>
2021-08-12 09:48:40 +0800

    C_3.1.1_2

--- Page_3.1.py
+++ Page_3.1.py
@@ -1,8 +1,36 @@
 
+# # --------------------------------- 1 ----------------------
+# import os
+# os.environ['KIVY_IMAGE'] = 'pil,sdl2'
+#
+#
+# from kivy.app import App
+# from kivy.uix.boxlayout import BoxLayout
+# from kivy.graphics import Rectangle, Color
+#
+#
+# class BoxLayoutWidget(BoxLayout):
+# 	def __init__(self, **kwargs):
+# 		super().__init__(**kwargs)
+#
+# 		# 设置背景
+# 		with self.canvas:
+# 			# 设置背景颜色, rgba格式, 通常值为0~1之间(具体的值/255)
+# 			Color(1, 1, 1, 1)
+# 			Rectangle(pos=self.pos, size=self.size)
+#
+# class Page3App(App):
+# 	def build(self):
+# 		return BoxLayoutWidget()
+#
+# if __name__ == "__main__":
+# 	Page3App().run()
 
+
+# --------------------------------- 2 ----------------------
 import os
-os.environ['KIVY_IMAGE'] = 'pil,sdl2'
 
+os.environ['KIVY_IMAGE'] = 'pil,sdl2'
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
@@ -17,11 +45,19 @@ class BoxLayoutWidget(BoxLayout):
 		with self.canvas:
 			# 设置背景颜色, rgba格式, 通常值为0~1之间(具体的值/255)
 			Color(1, 1, 1, 1)
-			Rectangle(pos=self.pos, size=self.size)
+			self.rect = Rectangle(pos=self.pos, size=self.size)
+			self.bind(pos=self.update_rect, size=self.update_rect)
 			
+	def update_rect(self, *args):
+		"""设置背景尺寸"""
+		self.rect.pos = self.pos
+		self.rect.size = self.size
+
+
 class Page3App(App):
 	def build(self):
 		return BoxLayoutWidget()
-	
+
+
 if __name__ == "__main__":
 	Page3App().run()
