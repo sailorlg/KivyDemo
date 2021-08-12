9d212a2158dba47935a57f23a5603668baefb16f
sailorlg <sailorlg@163.com>
2021-08-12 14:40:56 +0800

    C_3.3.3

--- Canvas33.py
+++ Canvas33.py
@@ -33,9 +33,49 @@
 # 	CanvasApp().run()
 
 
-# ------------------------------- 2 ---------------------------
+# # ------------------------------- 2 ---------------------------
+# import os
+# os.environ['KIVY_IMAGE'] = 'pil,sdl2'
+#
+# from kivy.app import App
+# from kivy.uix.relativelayout import RelativeLayout
+# from kivy.graphics import Rectangle, Color
+# from kivy.graphics.instructions import InstructionGroup
+#
+#
+# class RelativeLayoutWidget(RelativeLayout):
+# 	def __init__(self, **kwargs):
+# 		super().__init__(**kwargs)
+#
+# 		with self.canvas:
+# 			blue = InstructionGroup()
+# 			blue.add(Color(0, 0, 1, 0.2))
+# 			blue.add(Rectangle(pos=self.pos, size=(300, 300)))
+# 			self.canvas.add(blue)
+#
+# 			green = InstructionGroup()
+# 			green.add(Color(0, 1, 0, 0.4))
+# 			green.add(Rectangle(pos=(300, 300), size=(300, 300)))
+# 			self.canvas.add(green)
+#
+#
+# class CanvasApp(App):
+# 	def build(self):
+# 		return RelativeLayoutWidget()
+#
+#
+# if __name__ == "__main__":
+# 	CanvasApp().run()
+
+
+
+# ------------------------------- 3 ---------------------------
 import os
 os.environ['KIVY_IMAGE'] = 'pil,sdl2'
+
+from kivy.lang import Builder
+Builder.load_file('./kvs/canvas.kv')
+
 
 from kivy.app import App
 from kivy.uix.relativelayout import RelativeLayout
@@ -43,20 +83,10 @@ from kivy.graphics import Rectangle, Color
 from kivy.graphics.instructions import InstructionGroup
 
 
+
 class RelativeLayoutWidget(RelativeLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
-		
-		with self.canvas:
-			blue = InstructionGroup()
-			blue.add(Color(0, 0, 1, 0.2))
-			blue.add(Rectangle(pos=self.pos, size=(300, 300)))
-			self.canvas.add(blue)
-			
-			green = InstructionGroup()
-			green.add(Color(0, 1, 0, 0.4))
-			green.add(Rectangle(pos=(300, 300), size=(300, 300)))
-			self.canvas.add(green)
 
 
 class CanvasApp(App):
