9917b4f1c66084c2dd8dd033138a0fdd9b4b703d
sailorlg <sailorlg@163.com>
2021-08-13 14:52:32 +0800

    C_4.1.3

--- WidgetsSample4.py
+++ WidgetsSample4.py
@@ -15,12 +15,21 @@ from kivy.uix.boxlayout import BoxLayout
 class ButtonSampleWidget(BoxLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
+	
+	def press_button(self, arg):
+		"""按下按钮触发事件的回调函数"""
+		print("------press_button---------")
+		
+	def release_button(self, arg):
+		"""按下按钮并释放触发事件的回调函数"""
+		print("----------release_button--------------")
 		
 		
 
 class ButtonSampleApp(App):
 	def build(self):
-		return ButtonSampleWidget()
+		self.root_canvas = ButtonSampleWidget()
+		return self.root_canvas
 	
 
 if __name__ == "__main__":
