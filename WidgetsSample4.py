af2c4eb3be7a8ed4b40c2d0e5d316bfce1906646
sailorlg <sailorlg@163.com>
2021-08-17 08:29:10 +0800

    C_4.2.A-2

--- WidgetsSample4.py
+++ WidgetsSample4.py
@@ -35,37 +35,108 @@
 # 	ButtonSampleApp().run()
 
 
-# ------------------------- 1 ------------------------
+# # ------------------------- 2 ------------------------
+# import os
+# os.environ['KIVY_IMAGE'] = 'pil,sdl2'
+#
+# # from kivy.lang import Builder
+# # Builder.load_file('./kvs/labelrefer.kv')
+#
+# from kivy.app import App
+# from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.label import Label
+#
+# class LableBoxLayout(BoxLayout):
+# 	def __init__(self, **kwargs):
+# 		super().__init__(**kwargs)
+#
+# 		# 设置引用时, markup属性必须设置为真(True, 1, 等)
+# 		# 将"LABEL"文本标记, 单击"LABEL"文本时会触发绑定的事件, 单机"HELLO"文本则不会
+# 		label_ref = Label(text='HELLO [ref=label] LABEL [/ref] ', markup=True, color=(0.9, 0.2, 0.1, 1))
+#
+# 		# 绑定触发事件, 回调方法
+# 		label_ref.bind(on_ref_press=self.print_it)
+# 		self.add_widget(label_ref)
+#
+# 	# 未使用到self, 建议设置为静态方法
+# 	@staticmethod
+# 	def print_it(*args):
+# 		print("print_it is running~!")
+#
+# class LabelRefApp(App):
+# 	def build(self):
+# 		return LableBoxLayout()
+#
+# if __name__ == "__main__":
+# 	LabelRefApp().run()
+
+
+
+# # ------------------------- 3 ------------------------
+# import os
+#
+# os.environ['KIVY_IMAGE'] = 'pil,sdl2'
+#
+# # from kivy.lang import Builder
+# # Builder.load_file('./kvs/labelrefer.kv')
+#
+# from kivy.app import App
+# from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.label import Label
+#
+#
+# class LableBoxLayout(BoxLayout):
+# 	def __init__(self, **kwargs):
+# 		super().__init__(**kwargs)
+#
+# 		# 设置引用时, markup属性必须设置为真(True, 1, 等)
+# 		# 将"LABEL"文本标记, 单击"LABEL"文本时会触发绑定的事件, 单机"HELLO"文本则不会
+# 		label_ref = Label(text='HELLO [ref=label] LABEL [/ref] ', markup=True, color=(0.9, 0.2, 0.1, 1))
+#
+# 		# 绑定触发事件, 回调方法
+# 		label_ref.bind(on_ref_press=self.print_it)
+# 		self.add_widget(label_ref)
+#
+# 	# 未使用到self, 建议设置为静态方法
+# 	@staticmethod
+# 	def print_it(*args):
+# 		print("print_it is running~!")
+#
+#
+# class LabelRefApp(App):
+# 	def build(self):
+# 		return LableBoxLayout()
+#
+#
+# if __name__ == "__main__":
+# 	LabelRefApp().run()
+
+# ------------------------- 5 ------------------------
 import os
 os.environ['KIVY_IMAGE'] = 'pil,sdl2'
 
-# from kivy.lang import Builder
-# Builder.load_file('./kvs/labelrefer.kv')
+from kivy.lang import Builder
+Builder.load_file('./kvs/labelrefer.kv')
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
+
 class LableBoxLayout(BoxLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
-		
-		# 设置引用时, markup属性必须设置为真(True, 1, 等)
-		# 将"LABEL"文本标记, 单击"LABEL"文本时会触发绑定的事件, 单机"HELLO"文本则不会
-		label_ref = Label(text='HELLO [ref=label] LABEL [/ref] ', markup=True, color=(0.9, 0.2, 0.1, 1))
-		
-		# 绑定触发事件, 回调方法
-		label_ref.bind(on_ref_press=self.print_it)
-		self.add_widget(label_ref)
 		
 	# 未使用到self, 建议设置为静态方法
 	@staticmethod
 	def print_it(*args):
 		print("print_it is running~!")
-		
-class LabelRefApp(App):
+
+
+class LabelReferApp(App):
 	def build(self):
 		return LableBoxLayout()
 
+
 if __name__ == "__main__":
-	LabelRefApp().run()
+	LabelReferApp().run()
